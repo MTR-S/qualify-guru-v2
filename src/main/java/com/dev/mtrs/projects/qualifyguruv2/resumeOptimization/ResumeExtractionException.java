1676ca879d30355b09cfc9f064dd8d2267cd1979
MTR-S <matheusalmeidass14@gmail.com>
2026-08-06 17:10:48 -0300

    feat: Resume extraction using apache PDFBox API

---
+++ src/main/java/com/dev/mtrs/projects/qualifyguruv2/resumeOptimization/ResumeExtractionException.java
@@ -0,0 +1,7 @@
+package com.dev.mtrs.projects.qualifyguruv2.resumeOptimization;
+
+public class ResumeExtractionException extends RuntimeException {
+    public ResumeExtractionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/dev/mtrs/projects/qualifyguruv2/resumeOptimization/ResumeTextExtractor.java
@@ -0,0 +1,7 @@
+package com.dev.mtrs.projects.qualifyguruv2.resumeOptimization;
+
+import java.io.InputStream;
+
+public interface ResumeTextExtractor {
+    String extractText(InputStream fileStream);
+}

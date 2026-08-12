3cfd0b80bd4ec522f0c4e5e1e45f3aaacbbdcccb
MTR-S <matheusalmeidass14@gmail.com>
2026-08-11 21:50:07 -0300

    feat: Event Listener between Statistics and ResumeOptimization Modules

--- src/main/java/com/dev/mtrs/projects/qualifyguruv2/resumeOptimization/ResumeOptimizedEvent.java
+++ src/main/java/com/dev/mtrs/projects/qualifyguruv2/resumeOptimization/ResumeOptimizedEvent.java
@@ -1,4 +1,10 @@
 package com.dev.mtrs.projects.qualifyguruv2.resumeOptimization;
 
-public record ResumeOptimizedEvent() {
+import java.time.LocalDateTime;
+
+public record ResumeOptimizedEvent(
+        String jobTitle,
+        int compatibilityPercentage,
+        LocalDateTime optimizedAt
+) {
 }

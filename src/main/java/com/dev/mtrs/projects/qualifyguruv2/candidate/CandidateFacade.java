8dd8431e63704ad4fa0f4a378e1cb63ba1a01e26
MTR-S <matheusalmeidass14@gmail.com>
2026-08-17 11:57:38 -0300

    chore: Candidate old not used classes removed

--- src/main/java/com/dev/mtrs/projects/qualifyguruv2/candidate/CandidateFacade.java
+++
@@ -1,4 +0,0 @@
-package com.dev.mtrs.projects.qualifyguruv2.candidate;
-
-public class CandidateFacade {
-}
--- src/main/java/com/dev/mtrs/projects/qualifyguruv2/candidate/internal/CandidateRepository.java
+++
@@ -1,4 +0,0 @@
-package com.dev.mtrs.projects.qualifyguruv2.candidate.internal;
-
-public class CandidateRepository {
-}

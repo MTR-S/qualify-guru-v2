738a5e1f176d93857a93a7d44340fac0b9e656b8
MTR-S <matheusalmeidass14@gmail.com>
2026-08-19 11:22:46 -0300

    feat: SavedResume use cases and configurations

---
+++ src/main/java/com/dev/mtrs/projects/qualifyguruv2/candidateprofile/internal/adapters/out/SavedResumeRepository.java
@@ -0,0 +1,8 @@
+package com.dev.mtrs.projects.qualifyguruv2.candidateprofile.internal.adapters.out;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SavedResumeRepository extends JpaRepository<SavedResumeEntity, Long> {
+}

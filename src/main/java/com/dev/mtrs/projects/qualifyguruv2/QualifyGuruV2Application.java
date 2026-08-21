b3c5e288519594183afd595634ce6c1ec54b658a
MTR-S <matheusalmeidass14@gmail.com>
2026-08-21 11:41:44 -0300

    fix: Fixing Flyway migration setup

--- src/main/java/com/dev/mtrs/projects/qualifyguruv2/QualifyGuruV2Application.java
+++ src/main/java/com/dev/mtrs/projects/qualifyguruv2/QualifyGuruV2Application.java
@@ -1,7 +1,11 @@
 package com.dev.mtrs.projects.qualifyguruv2;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Encoders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.crypto.SecretKey;
 
 @SpringBootApplication
 public class QualifyGuruV2Application {
--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -1,32 +1,32 @@
 -- V1__init_schema.sql
 CREATE TABLE event_publication (
-                                   id VARCHAR(36) NOT NULL,
-                                   listener_id VARCHAR(512) NOT NULL,
-                                   event_type VARCHAR(512) NOT NULL,
-                                   serialized_event VARCHAR(4000) NOT NULL,
-                                   publication_date TIMESTAMP NOT NULL,
-                                   completion_date TIMESTAMP,
-                                   status VARCHAR(255) NOT NULL,
-                                   completion_attempts INTEGER NOT NULL,
-                                   last_resubmission_date TIMESTAMP,
-                                   PRIMARY KEY (id)
+    id VARCHAR(36) NOT NULL,
+    listener_id VARCHAR(512) NOT NULL,
+    event_type VARCHAR(512) NOT NULL,
+    serialized_event VARCHAR(4000) NOT NULL,
+    publication_date TIMESTAMP NOT NULL,
+    completion_date TIMESTAMP,
+    status VARCHAR(255) NOT NULL,
+    completion_attempts INTEGER NOT NULL,
+    last_resubmission_date TIMESTAMP,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE stat_optimization_metrics (
-                                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                           job_title VARCHAR(255) NOT NULL,
-                                           compatibility_percentage INT NOT NULL,
-                                           optimized_at TIMESTAMP NOT NULL
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(255) NOT NULL,
+    compatibility_percentage INT NOT NULL,
+    optimized_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE auth_users (
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            email VARCHAR(255) NOT NULL UNIQUE,
-                            password VARCHAR(255) NOT NULL
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE cand_candidates (
-                                 id BIGINT PRIMARY KEY
+    id BIGINT PRIMARY KEY
 );
 
 CREATE TABLE cand_saved_resumes (
